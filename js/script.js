539dfca3e22ec5110b0ea574221f622723739a31
kinyua254 <victor.mutugi@student.moringaschool.com>
2021-11-23 01:03:02 +0300

    the toogle effect on what we do

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$('.developments').click(function(){
+    $('#ddevelopment').slideToggle();
+    $('.developments').slideToggle();
+
+});
+$('.imgdesign').click(function(){
+    $('#design').slideToggle();
+    $('.imgdesign').slideToggle();
+});
+$('.promanagement').click(function(){
+    $('#product').slideToggle();
+    $('.promanagement').slideToggle();
+});
